27cba1dfcdf9e9f4995f3ff055a4f5627515883c
kushkansagara <110079442+kushkansagara@users.noreply.github.com>
2023-10-11 18:31:33 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,30 +1,32 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const cors = require('cors')
-const icecreamroute = require("./routes/icecream")
-const userroute = require("./routes/user")
-const cartRoute=require("./routes/cart")
-const orderRoute = require("./routes/order")
+const cors = require("cors");
+const icecreamroute = require("./routes/icecream");
+const userroute = require("./routes/user");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
-require('./db/conn')
+require("./db/conn");
 require("dotenv").config();
 
-
-app.use(express.json())
-// app.use(
-//     cors({
-//         origin: "http://localhost:3000"
-//         // methods: "GET,POST,PUT,DELETE",
-//     })
-// );
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://devnarayan.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    candentials: true,
+  })
+);
 
 app.use(cors());
 
 app.use("/", userroute);
-app.use("/icecream/", icecreamroute)
-app.use("/cart/",cartRoute);
+app.use("/icecream/", icecreamroute);
+app.use("/cart/", cartRoute);
 app.use("/order/", orderRoute);
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log("Backend is running on port", port))
-
+// const port = process.env.PORT || 8080;
+// app.listen(port, () => console.log("Backend is running on port", port));
+app.listen(3001, () => {
+  console.log("Server is Running");
+});
