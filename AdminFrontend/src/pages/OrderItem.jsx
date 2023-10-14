fb28ed1835771c3815892f675e589cd47e4cdaa2
kushkansagara <110079442+kushkansagara@users.noreply.github.com>
2023-10-14 13:24:06 +0530

    Update OrderItem.jsx

--- AdminFrontend/src/pages/OrderItem.jsx
+++ AdminFrontend/src/pages/OrderItem.jsx
@@ -7,7 +7,7 @@ function OrderItem({ count, m }) {
     useEffect(()=>{
         const getProduct = async (m) => {
             try {
-                const res = await axios.get(`http://localhost:8080/icecream/ice/${m.icecream}`);
+                const res = await axios.get(`https://devnarayan.onrender.com/icecream/ice/${m.icecream}`);
                 setProduct(res.data);
                 // console.log(res.data);
                 return res.data;
