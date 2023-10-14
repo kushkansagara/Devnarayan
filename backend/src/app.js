b674a89a0cf0517b0616ff95365cbc65544b2433
kushkansagara <110079442+kushkansagara@users.noreply.github.com>
2023-10-14 11:55:11 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,41 +1,28 @@
 const express = require("express");
+const app = express();
+
 const cors = require("cors");
 const icecreamroute = require("./routes/icecream");
 const userroute = require("./routes/user");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 
-// Import dotenv and configure it for production
-require("dotenv").config({ path: "./.env.local" });
-
-// Replace the following line with your database connection logic
 require("./db/conn");
+require("dotenv").config();
 
-const app = express();
-
-// Configure CORS
-const corsOptions = {
-  origin: "https://devnarayan.vercel.app",
-  // methods: "GET,POST,PUT,DELETE",
-};
-
-app.use(cors(corsOptions));
-
-// Body parser middleware
 app.use(express.json());
+// app.use(
+//     cors({
+//         origin: "http://localhost:3000"
+//         // methods: "GET,POST,PUT,DELETE",
+//     })
+// );
 
-// Define routes
+app.use(cors());
+
 app.use("/", userroute);
 app.use("/icecream/", icecreamroute);
 app.use("/cart/", cartRoute);
 app.use("/order/", orderRoute);
-
-// Define a default route or handle 404 errors
-app.use((req, res) => {
-  res.status(404).json({ error: "Not Found" });
-});
-
 const port = process.env.PORT || 8080;
-
-// Start the server
 app.listen(port, () => console.log("Backend is running on port", port));
