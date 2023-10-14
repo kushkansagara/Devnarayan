1353e8959e3d868414dd97d453f45d31d1808a30
kushkansagara <110079442+kushkansagara@users.noreply.github.com>
2023-10-14 10:38:39 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,13 +11,12 @@ require("./db/conn");
 require("dotenv").config();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://devnarayan.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    candentials: true,
-  })
-);
+// app.use(
+//     cors({
+//         origin: "http://localhost:3000"
+//         // methods: "GET,POST,PUT,DELETE",
+//     })
+// );
 
 app.use(cors());
 
@@ -25,8 +24,5 @@ app.use("/", userroute);
 app.use("/icecream/", icecreamroute);
 app.use("/cart/", cartRoute);
 app.use("/order/", orderRoute);
-// const port = process.env.PORT || 8080;
-// app.listen(port, () => console.log("Backend is running on port", port));
-app.listen(3001, () => {
-  console.log("Server is Running");
-});
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log("Backend is running on port", port));
